26c1e81a3b189148e077366c69653af3af8cabdf
Shailesh Tayde <shaileshstayde2004@gmail.com>
2026-09-03 19:21:41 +0530

    Fix syntax error in app.py page configuration

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ from tensorflow.keras.preprocessing import image
 from streamlit_option_menu import option_menu
 st.set_page_config(page_title='PulmoNova AI'
                   page_icon='🫁',
-                  layout='wide')
+                  layout='wide'
+)
 #Loading models
 cancer_model = pickle.load(open('models/final_model.sav', 'rb'))
 
